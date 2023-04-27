3b740fa92d7b757083f624073508b49bb8dc9c41
n1nj4sec <n1nj4sec@users.noreply.github.com>
2023-04-27 15:35:40 +0200

    Update example-iptables.sh

--- example-iptables.sh
+++ example-iptables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
-iptables -A OUTPUT -o lo -j ACCEPT
+iptables -t nat -A OUTPUT -o lo -j ACCEPT
 iptables -t nat -A OUTPUT -d 10.0.0.0/8 -p tcp -m tcp -j REDIRECT --to-ports 10080
 iptables -t nat -A OUTPUT -p udp -m udp --dport 53 -j REDIRECT --to-ports 10053
